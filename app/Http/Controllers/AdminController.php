c481e454886c5f97f5b806ad0e8b1d2107f447f3
ashwini1966 <bhawalkarashwini1996@gmail.com>
2024-10-04 17:56:00 +0530

    push project

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index(){
+
+        $date = Carbon::now();
+
+        return $date;
+    }
+}
